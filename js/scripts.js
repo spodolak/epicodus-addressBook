70ee9f6c86130aedb68da620565ea684561bdeb5
Dusty <dustymccord@gmail.com>
2020-01-24 19:59:00 -0800

    make the js code  more pretty.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-  console.log('JavaScipt is working');
+$(document).ready(function() {
+  console.log("JavaScipt is working");
 
   //on click function
-  $('form').submit(function(event){
+  $("form").submit(function(event) {
     event.preventDefault();
-    console.log('click');
+    console.log("click");
   });
-  
 });
