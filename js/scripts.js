2dcd0192c22b792af94e41f5b7b346bb6d7d244d
Dusty <dustymccord@gmail.com>
2020-01-26 22:44:44 -0800

    add a find function, a delete function, and an update function.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,50 @@ $(document).ready(function() {
 	//Business Logic for AddressBook --------
 	function AddressBook() {
 		this.contacts = [];
+		this.currentId = 0;
 	}
 
 	AddressBook.prototype.addContact = function(contact) {
+		contact.id = this.assignId();
 		this.contacts.push(contact);
+	};
+
+	AddressBook.prototype.assignId = function() {
+		this.currentId += 1;
+		return this.currentId;
+	};
+
+	AddressBook.prototype.findContact = function(id) {
+		for (let i = 0; i < this.contacts.length; i++) {
+			if (this.contacts[i]) {
+				if (this.contacts[i].id == id) {
+					return this.contacts[i];
+				}
+			}
+		}
+		return false;
+	};
+
+	AddressBook.prototype.updateContact = function(id, object) {
+		for (let i = 0; i < this.contacts.length; i++) {
+			if (this.contacts[i].id == id) {
+				this.contacts[i].firstName = object.firstName;
+				this.contacts[i].lastName = object.lastName;
+				this.contacts[i].phoneNumber = object.phoneNumber;
+			}
+		}
+	};
+
+	AddressBook.prototype.deleteContact = function(id) {
+		for (let i = 0; i < this.contact.length; i++) {
+			if (this.contacts[i]) {
+				if (this.contacts[i].id == id) {
+					delete this.contacts[i];
+					return true;
+				}
+			}
+		}
+		return false;
 	};
 
 	//Business Logic for Contacts -----------
