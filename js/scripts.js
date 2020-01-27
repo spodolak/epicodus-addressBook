7cdad2584fc81e90cfa4521b88a973b2d050ff15
Dusty <dustymccord@gmail.com>
2020-01-26 22:18:05 -0800

    add an AddressBook constructor function and a Contacts constructor function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
 	console.log('JavaScipt is working');
 
+	//Business Logic for AddressBook --------
+	function AddressBook() {
+		this.contacts = [];
+	}
+
+	AddressBook.prototype.addContact = function(contact) {
+		this.contacts.push(contact);
+	};
+
+	//Business Logic for Contacts -----------
+	function Contact(firstName, lastName, phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+	}
+
+	Contact.prototype.fullName = function() {
+		return this.firstName + ' ' + this.lastName;
+	};
+
 	//on click function
-	$('form').submit(function(event) {
-		event.preventDefault();
-		console.log('click');
-	});
+	// $('form').submit(function(event) {
+	// 	event.preventDefault();
+	// 	console.log('click');
+	// });
 });
